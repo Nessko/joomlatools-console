7b731c1ff08673642fcc5f4ccc401e57dfe55efc
xcidar <xcidar@gmail.com>
2016-05-16 15:43:07 +0200

    change namespace

--- src/Joomlatools/Console/Command/Language/ListAll.php
+++ src/Joomlatools/Console/Command/Language/ListAll.php
@@ -1,5 +1,5 @@
 <?php
-namespace Joomlatools\Console\Command\Site;
+namespace Joomlatools\Console\Command\Language;
 
 use Symfony\Component\Console\Command\Command
 use Symfony\Component\Console\Input\ArrayInput;
